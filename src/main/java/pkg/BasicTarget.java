f48a6218766b141615515971bff80a0e15deb820
almiraj <rain.almiraj@gmail.com>
2023-05-26 20:09:13 +0900

    build base

--- src/main/java/pkg/BasicTarget.java
+++ src/main/java/pkg/BasicTarget.java
@@ -11,24 +11,26 @@ public class BasicTarget {
 	@SuppressWarnings("unused")
 	private String privateFieldValue = "privateFieldValue!";
 
-	public String basicTarget() {
+	public String mainMethod() {
 		StringBuilder sb = new StringBuilder();
 
-		this.publicVoidMethod("publicVoidMethodArg");
-		sb.append(this.publicReturnMethod("publicReturnMethodArg"));
+		this.publicVoidMethod("publicVoidMethodArg!");
+		sb.append(this.publicReturnMethod("publicReturnMethodArg!"));
+		sb.append("\n");
 
-		this.privateVoidMethod("privateVoidMethodArg");
-		sb.append(this.privateReturnMethod("privateReturnMethodArg"));
+		this.privateVoidMethod("privateVoidMethodArg!");
+		sb.append(this.privateReturnMethod("privateReturnMethodArg!"));
+		sb.append("\n");
 
 		return sb.toString();
 	}
 
 	public void publicVoidMethod(String arg) {
-		this.wrap("publicVoidMethod! [arg=" + arg + "]");
+		this.wrap("publicVoidMethod! [args=" + arg + "]");
 	}
 
 	public String publicReturnMethod(String arg) {
-		return "publicReturnMethod! [arg=" + arg + "]";
+		return "publicReturnMethod! [args=" + arg + "]";
 	}
 
 	public void wrap(String src) {
@@ -36,10 +38,10 @@ public class BasicTarget {
 	}
 
 	private void privateVoidMethod(String arg) {
-		this.wrap("privateVoidMethod! [arg=" + arg + "]");
+		this.wrap("privateVoidMethod! [args=" + arg + "]");
 	}
 
 	private String privateReturnMethod(String arg) {
-		return "privateReturnMethod! [arg=" + arg + "]";
+		return "privateReturnMethod! [args=" + arg + "]";
 	}
 }
--- src/test/java/pkg/BasicTargetTest.java
+++ src/test/java/pkg/BasicTargetTest.java
@@ -1,14 +1,54 @@
 package pkg;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doReturn;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
 
 public class BasicTargetTest {
 
+	private AutoCloseable mock;
+
+	@Before
+	public void setUp() {
+		this.mock = MockitoAnnotations.openMocks(this);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		this.mock.close();
+	}
+
+	@Spy
+	private BasicTarget basicTarget;
+
 	@Test
-	public void test() {
-		fail("まだ実装されていません");
+	public void testMainMethod_nonMock() {
+		String actual = this.basicTarget.mainMethod();
+
+		String expected =
+				"publicReturnMethod! [args=publicReturnMethodArg!]\n"
+				+ "privateReturnMethod! [args=privateReturnMethodArg!]\n";
+
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void testMainMethod_mocked() {
+		doReturn("publicReturnMethod? [---]").when(this.basicTarget).publicReturnMethod(any());
+
+		String actual = this.basicTarget.mainMethod();
+
+		String expected =
+				"publicReturnMethod? [---]\n"
+				+ "privateReturnMethod! [args=privateReturnMethodArg!]\n";
+
+		assertEquals(expected, actual);
 	}
 
 }
